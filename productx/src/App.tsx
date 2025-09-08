823f98261c89d635dc02bd19e3aeed75a909fd18
Aiviraj Rajput <aiviraj.rajput@qcentrio.com>
2025-09-08 16:35:53 +0530

    first commit

---
+++ productx/src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+import './index.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1 className='text-4xl text-red-500'>Abhiraj Singh</h1>
+      </div>
+      
+    </>
+  )
+}
+
+export default App
