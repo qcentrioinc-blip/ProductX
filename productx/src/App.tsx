5342cd6ea11e782daf95f125dd22930fd2eda6bb
Aiviraj Rajput <aiviraj.rajput@qcentrio.com>
2025-09-10 11:52:59 +0530

    homepage folder

--- productx/src/App.tsx
+++ productx/src/App.tsx
@@ -1,4 +1,5 @@
 import './App.css'
+import HeroSection from './components/Homepage/HeroSection'
 import './index.css'
 
 function App() {
@@ -6,7 +7,7 @@ function App() {
   return (
     <>
       <div>
-        <h1 className='text-4xl text-red-500'>Kalpana Patwal as a developer</h1>
+        <HeroSection />
       </div>
       
     </>
