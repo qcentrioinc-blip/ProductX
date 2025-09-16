2be0bc77972c71c732dad8bd622c890dea6c24fd
Aiviraj Rajput <aiviraj.rajput@qcentrio.com>
2025-09-16 16:13:10 +0530

    navbar + routes

---
+++ productx/src/routes/industries/HighTech.tsx
@@ -0,0 +1,10 @@
+import InsightThought from "../../components/HighTech/InsightThought"
+import LandingPage from "../../components/HighTech/LandingPage"
+
+const HighTech = () => (
+  <div>
+    <InsightThought />
+    <LandingPage />
+  </div>
+)
+export default HighTech
