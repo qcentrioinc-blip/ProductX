de935ebe2d3f20e3911389b4cc9356bd8bf57ded
KalpanaPatwalqcentrio <kalpana.patwal@qcentrio.com>
2025-09-19 18:07:50 +0530

    new section in industrial page

--- productx/src/routes/industries/HighTech.tsx
+++ productx/src/routes/industries/HighTech.tsx
@@ -1,21 +1,19 @@
 import InsightThought from "../../components/HighTech/InsightThought"
-// import LandingPage from "../../components/HighTech/LandingPage"
 import HowWeDoIn from "../../components/HighTech/HowWeDoIn"
 import Footer from "../../components/Footer/Footer"
 import Process from "../../components/HighTech/Process"
 import WhatWeDoIn from "../../components/HighTech/WhatWeDoIn"
 import Partnership from "../../components/HomePage/Partnership"
-import LandingPage from "../../components/HighTech/LandingPage"
-import LandingPag from "../../components/HomePage/LandingPage"
+import LandingPage from "../../components/HomePage/LandingPage"
 const HighTech = () => (
   <div>
-    <LandingPag/>
+    <LandingPage/>
     <Partnership/>
     <HowWeDoIn/>
     <WhatWeDoIn/>
     <InsightThought />
     <Process />
-    <LandingPage />
+  
     <Footer />
   </div>
 )
