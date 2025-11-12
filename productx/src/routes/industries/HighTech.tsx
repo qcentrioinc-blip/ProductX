7da46d28a36cb0778b9f61160dcfccdd98b1b70e
Aravind-Qcentrio <aravind@qcentrio.com>
2025-11-12 12:29:22 +0530

    commented careers section from landing page in HighTech.

--- productx/src/routes/industries/HighTech.tsx
+++ productx/src/routes/industries/HighTech.tsx
@@ -1,11 +1,11 @@
-import CareersPage from "../../components/HighTech/Careers/CareersPage"
+// import CareersPage from "../../components/HighTech/Careers/CareersPage"
 import HeroSectionHighTech from "../../components/HomePage/HighTech/HeroSectionHighTech"
      
 
 
 const HighTech = () => (
   <div>
-    <CareersPage/>
+    {/* <CareersPage/> */}
     <HeroSectionHighTech/>
   </div>
 )
