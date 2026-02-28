eea92b5c6604156696199b9c967424858ec4fd3b
KalpanaPatwalqcentrio <kalpana.patwal@qcentrio.com>
2026-02-28 19:13:24 +0530

    platorm remove ehr footer submit

--- productx/src/components/EHR&PMS/Navbar/EHRNavbar.tsx
+++ productx/src/components/EHR&PMS/Navbar/EHRNavbar.tsx
@@ -216,8 +216,8 @@ const EHRNavbar = () => {
         </Link>
 
         <div className="hidden lg:flex items-center gap-6">
-          <Link to={`${base}/platform`} className={`font-medium transition-colors ${isScrolled ? 'text-black' : 'text-white'}`}>Platform</Link>
-          <Link to={`${base}/marketplace`} className={`font-medium transition-colors ${isScrolled ? 'text-black' : 'text-white'}`}>Marketplace</Link>
+          {/* <Link to={`${base}/platform`} className={`font-medium transition-colors ${isScrolled ? 'text-black' : 'text-white'}`}>Platform</Link> */}
+          <Link to={`${base}/marketplace`} className={`font-medium transition-colors ${isScrolled ? 'text-black' : 'text-black'}`}>Marketplace</Link>
         </div>
 
         <button
@@ -566,7 +566,7 @@ const EHRNavbar = () => {
         </div>
 
         <div className="flex justify-between mt-10 gap-6 pt-4">
-          <Link to={`${base}/platform`} onClick={() => setMenuOpen(false)} className="text-purple-600 text-lg font-semibold">Platform</Link>
+          {/* <Link to={`${base}/platform`} onClick={() => setMenuOpen(false)} className="text-purple-600 text-lg font-semibold">Platform</Link> */}
           <Link to={`${base}/marketplace`} onClick={() => setMenuOpen(false)} className="text-purple-600 text-lg font-semibold">Marketplace</Link>
         </div>
 
